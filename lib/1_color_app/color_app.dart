2f85b94fc856d982596316bd90bfbf1d7918f400
vathanakphy <vathanakphy@gmail.com>
2026-02-19 10:00:25 +0700

    TASK-001 – The Color App

--- lib/1_color_app/color_app.dart
+++ lib/1_color_app/color_app.dart
@@ -9,7 +9,29 @@ void main() {
   );
 }
 
-enum CardType { red, blue }
+enum CardType {
+  red(Colors.red),
+  blue(Colors.blue),
+  green(Colors.green),
+  yellow(Colors.yellow);
+
+  final Color color;
+  const CardType(this.color);
+}
+
+class ColorService extends ChangeNotifier {
+  final Map<CardType, int> _counts = Map.fromEntries(
+    CardType.values.map((e) => MapEntry(e, 0)),
+  );
+  int getCount(CardType color) => _counts[color] ?? 0;
+
+  void increment(CardType color) {
+    _counts[color] = (_counts[color] ?? 0) + 1;
+    notifyListeners();
+  }
+}
+
+ColorService colorService = ColorService();
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,36 +42,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
-  int redTapCount = 0;
-  int blueTapCount = 0;
-
-  void _incrementRedTapCount() {
-    setState(() {
-      redTapCount++;
-    });
-  }
-
-  void _incrementBlueTapCount() {
-    setState(() {
-      blueTapCount++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _currentIndex == 0
-              ? ColorTapsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-                onRedTap: _incrementRedTapCount,
-                onBlueTap: _incrementBlueTapCount,
-              )
-              : StatisticsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-              ),
+      body: _currentIndex == 0 ? ColorTapsScreen() : StatisticsScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -73,29 +70,22 @@ class _HomeState extends State<Home> {
 }
 
 class ColorTapsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-  final VoidCallback onRedTap;
-  final VoidCallback onBlueTap;
-
-  const ColorTapsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-    required this.onRedTap,
-    required this.onBlueTap,
-  });
+  const ColorTapsScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Color Taps')),
       body: Column(
         children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(
-            type: CardType.blue,
-            tapCount: blueTapCount,
-            onTap: onBlueTap,
+          ...CardType.values.map(
+            (color) => ListenableBuilder(
+              listenable: colorService,
+              builder: (context, child) => ColorTap(
+                type: color,
+                tapCount: colorService.getCount(color),
+                onTap: () => colorService.increment(color),
+              ),
+            ),
           ),
         ],
       ),
@@ -115,7 +105,7 @@ class ColorTap extends StatelessWidget {
     required this.onTap,
   });
 
-  Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
+  Color get backgroundColor => type.color;
 
   @override
   Widget build(BuildContext context) {
@@ -141,26 +131,26 @@ class ColorTap extends StatelessWidget {
 }
 
 class StatisticsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-
-  const StatisticsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-  });
+  const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Statistics')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
-          ],
+      body: ListenableBuilder(
+        listenable: colorService,
+        builder: (context, child) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ...CardType.values.map(
+                (color) => Text(
+                  '${color.name} Taps: ${colorService.getCount(color)}',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
